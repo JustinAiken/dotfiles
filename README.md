471a24de2226d0c51f9e7cd2efc13d67d3fe3830
Justin Aiken <60tonangel@gmail.com>
2013-01-13 10:00:49 -0700

    Switch to symlinks instead of trying to gitignore everything in ~/

---
+++ README.md
@@ -0,0 +1,3 @@
+git clone https://github.com/JustinAiken/dotfiles.git
+chmod +x ~/dotfiles/sync.sh
+~/dotfiles/sync.sh
---
+++ sync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dir=~/dotfiles                    # dotfiles directory
+files="gitignore gitconfig githelpers vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+for file in $files; do
+	echo "Killing $file"
+	rm -rf ~/.$file 
+	echo "Creating symlink to $file in home directory."
+	ln -s $dir/.$file ~/.$file
+done
