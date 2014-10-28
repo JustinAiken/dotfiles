a6e8fc563602d0ceec01a89a2449b51fbaeb6912
Justin Aiken <60tonangel@gmail.com>
2014-10-28 16:48:46 -0600

    Clean up host completetion

--- .oh-my-zsh-custom/ssh.zsh
+++ .oh-my-zsh-custom/ssh.zsh
@@ -1,3 +1,4 @@
-h=($h ${${${(@M)${(f)"$(cat ~/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
+h=(${${${(@M)${(f)"$(cat ~/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
 
 zstyle ':completion:*:(ssh|scp|sftp|rsh|rsync):*' hosts $h
+zstyle ':completion:*:(ssh|scp|sftp|rsh|rsync):*' ignored-patterns 'ec2*.compute.amazonaws.com' 'heroku.*'
