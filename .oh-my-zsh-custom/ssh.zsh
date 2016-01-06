3ff816921f5a5186ec570432d2d6595e8ca224cd
Justin Aiken <60tonangel@gmail.com>
2016-01-05 17:19:48 -0700

    Less annoying host completion

--- .oh-my-zsh-custom/ssh.zsh
+++ .oh-my-zsh-custom/ssh.zsh
@@ -1,4 +1,4 @@
-h=(${${${(@M)${(f)"$(cat ~/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
+h=(${${(s: :)${(ps:\t:)${${(@M)${(f)"$(<$HOME/.ssh/config)"}:#Host *}#Host}}}:#*[*?]*})
 
 zstyle ':completion:*:(ssh|scp|sftp|rsh|rsync):*' hosts $h
 zstyle ':completion:*:(ssh|scp|sftp|rsh|rsync):*' ignored-patterns 'ec2*.compute.amazonaws.com' 'heroku.*'
