a52284c4914be7b4bfb21577e2be5dd63445a789
Justin Aiken <60tonangel@gmail.com>
2017-03-15 14:26:10 -0600

    Add an alias for my daily brewing

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -3,3 +3,14 @@ alias -g db:redo="db:drop db:create db:migrate db:seed"
 railsnew() {
   rails new "$*" --skip-turbolinks --skip-test-unit
 }
+
+brewmeister() {
+  brew update --force
+  brew upgrade
+  echo "Cleaning up!\n\n"
+  brew cleanup --outdated
+  brew cask cleanup --outdated
+  echo "Outdated: \n\n"
+  brew-cask-outdated
+  echo "Install outdateds with `brew cask reinstall`"
+}
