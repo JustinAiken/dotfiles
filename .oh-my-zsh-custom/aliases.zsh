280c31d8a29e99ff182b06b3e83b1e3fcbfa3a78
Justin Aiken <60tonangel@gmail.com>
2017-04-14 15:08:58 -0600

    Everytime brew updates node I have to redo this

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -14,3 +14,18 @@ brewmeister() {
   brew-cask-outdated
   echo "Install outdateds with \`brew cask reinstall\`"
 }
+
+fixpow() {
+  echo "Reinstalling"
+  sudo pow --install-system
+  pow --install-local
+  
+  echo "Fixing firewall"
+  sudo launchctl unload -w /Library/LaunchDaemons/cx.pow.firewall.plist
+  sudo launchctl load -w /Library/LaunchDaemons/cx.pow.firewall.plist
+
+  echo "Fixing service"
+  launchctl unload -w ~/Library/LaunchAgents/cx.pow.powd.plist
+  launchctl load -w ~/Library/LaunchAgents/cx.pow.powd.plist
+}
+
